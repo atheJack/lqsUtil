2d1513917f9c289eb2f32f5455f35c637bc57cba
jackeyluo <jackeyluo@tencent.com>
2021-05-25 17:05:06 +0800

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/LqsUtil.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+public class LqsUtil {
+    public static int add(int a,int b){
+        return a+b;
+    }
+
+    public static String getMessage(){
+        return "hello,world";
+    }
+}
